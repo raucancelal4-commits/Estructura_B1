6f17e988f9c0d0a0580974f7c9b69409e2f1524b
Ronny Aucancela <raucancelal4@unemi.edu.ec>
2026-09-10 19:48:43 -0500

    Ingreso de los 5 pasos

--- Js vs Python/Ejercicios.py
+++ Js vs Python/Ejercicios.py
@@ -1,8 +1,42 @@
-
+'''Ejercicio 4:  Convertir grados Celsius a Fahrenheit
+1.- Entender el problema
+Entrada:
+Celsius (input)
+Proceso
+F= Celsius * 9 / 5 + 32 -> Una formula
+Salida
+Fahrenheit
+2.- Bosquejo a mano 
+Celsius = 25.5
+Paso 1= F: 25.5 * 9 / 5 + 32 :77.9°F
+3.- descubrir patrón
+Hay 1 entrada y 1 salida, no hay bucles ni decisiones, como puede venir decimales 
+como Celsius de 25.5 es válida por que utilizamos el float
+4.- Código'''
 celsius = float(input('Ingrese temperatura: '))
 F = celsius * 9/5 + 32
 print(f"La temperatura es: {F:.1f} °F")
 
+'''Ejercicio 5: Segundos a horas, minutos y segundos. 
+1.- Entender el problema
+Entrada:
+Total = (input)
+Proceso
+Horas = total // 3600
+Resto = total % 3600
+Minutos= resto //60
+Segundos = resto % 60
+Salida
+hh:mm:ss
+2.- Bosquejo a mano 
+Total = 3725
+Paso 1= horas = 3725 // 3600 = 1
+Paso 2= resto = 3725 % 3600 = 125
+Paso 3= minutos = 125 // 60 = 2
+Paso 4= segundo = 125 % 60 = 5 
+3.- descubrir patrón
+Tiene 1 entrada y 1 salida; no utiliza bucles ni decisiones. Usa int porque los segundos se ingresan como números enteros.
+4.- Código'''
 total= int (input('Ingrese el total de segundos: '))
 horas = total // 3600
 resto = total % 3600
@@ -10,11 +44,42 @@ minutos = resto // 60
 segundos = resto % 60
 print(f"La hora es: {horas}:{minutos:02d}:{segundos:02d}")
 
+'''Ejercicio 6: Intercambio de variable
+1.- Entender el problema
+Entrada:
+a= (input), b = (input)
+Proceso
+a , b = b , a
+Salida
+a = a
+b= b
+2.- Bosquejo a mano 
+a = 10 ; b = 5
+a = 5 ; b = 10
+3.- descubrir patrón
+Tiene 2 entradas y 1 salida, no utiliza bucles ni decisiones, y realiza un intercambio de valores entre las variables a y b.
+4.- Código'''
 a = int(input('a: '))
 b = int(input('b: '))
 a, b = b , a
 print(f"a: {a} y b: {b}")
 
+'''Ejercicio 7: Calcular el Iva (15% Ecuador) 
+1.- Entender el problema
+Entrada:
+Precio (input)
+Proceso
+iva= precio * 0.15
+Total = precio + iva
+Salida
+Total del precio con iva
+2.- Bosquejo a mano 
+precio= 150
+iva = 150 * 0.15 = 22.50
+total = 150 + 22.50 = 172.50
+3.- descubrir patrón
+Tiene 1 entrada y 2 salidas: muestra el valor del IVA y el precio total con IVA. Utilizamos float porque el precio puede contener decimales y no hay condiciones ni bucles.
+4.- Código'''
 precio = float(input('Precio sin Iva: '))
 IVA = 0.15
 iva = precio * IVA
--- Js vs Python/Taller.py
+++ Js vs Python/Taller.py
@@ -1,7 +1,41 @@
+'''Ejercicio 1: Saludo personalizado
+1.- Entender el problema
+Entrada:
+Nombre = (input), Edad = (input)
+Proceso
+Concatenar el hola, con el nombre y la edad
+Salida
+El saludo completo
+2.- Bosquejo a mano 
+Nombre = “Ronny”, Edad = 18
+Salida: Hola mi nombre es Ronny y tengo 18 años. Bienvenido al curso
+3.- descubrir patrón
+Hay solo un dato de entrada y no hay más que hacer cuentas solo formar un mensaje. 
+Este es el patrón más simple leer -> mostrar
+4.- Código'''
 nombre = input('Ingrese su nombre: ')
 edad = int (input('Ingrese su edad: '))
 print(f"Hola mi nombre es {nombre} y tengo {edad} años, Bienvenido al curso.")
 
+'''Ejercicio 2: Promedio de 3 notas
+1.- Entender el problema
+Entrada:
+n1,n2,n3 (input)
+Proceso
+Sumar las 3 y dividir para 3
+Salida
+EL promedio
+2.- Bosquejo a mano 
+n1 = 8, n2=6, n3 = 10
+Paso 1= sumo 8+6+10=24
+Paso 2= divido 24/3= 8.0
+Paso 3 = muestro “Promedio: 8.0”
+3.- descubrir patrón
+Tiene 2 pasos primero suma y después divide, el paréntesis obligatorio 
+sin ellos solo se divide n3 para 3, El 3 no se lee es un valor fijo del problema 
+(siempre son 3 notas),solo lee lo que el usuario decide. 
+En Python / siempre da float (8.0, no 8). Perfecto el promedio casi siempre da decimales.
+4.- Código'''
 n1 = float(input('Nota 1: '))
 n2= float(input('Nota 2: '))
 n3 = float(input('Nota 3: '))
@@ -10,6 +44,24 @@ print(f"Promedio: {promedio:.1f}")
 if promedio >= 7 : print("AProbado")
 else : print("reprobado")
 
+'''Ejercicio 3: Area y perimetro de un rectangulo.
+1.- Entender el problema
+Entrada:
+Base,altura (input)
+Proceso
+Aplica 2 formulas
+Salida
+El área y el perimetro
+2.- Bosquejo a mano 
+Base = 5, altura= 3
+Área = 5 * 3 = 15
+Perímetro = 2 * (5 + 3) = 16
+3.- descubrir patrón
+2 entradas y 2 salidas, independientes cada formula usa base
+Y altura por separado, no hay bucles ni decisiones.
+Como pueden venir decimales (una base de .5 cm es valida),
+Usamos el float
+'''
 import math
 radio = int(input('Ingrese el radio: '))
 area = math.pi * (radio ** 2)
